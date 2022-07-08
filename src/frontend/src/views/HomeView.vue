c3caa77a59cf60e075ad9e9d6a322bdcf387fc6c
elmi-elmi <shahrokhelmi@gmail.com>
2022-07-08 22:09:39 +0430

    autofocus

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,84 +1,84 @@
 <template>
   <div>
-  <v-card
-      class="d-flex align-center flex-column mx-auto pa-4 mb-1"
-      max-width="400px"
-      min-height="300px"
-      outlined
-      elevation="18"
-  >
-    <v-list-item three-line
-                 class="mb-12"
-    >
-      <v-list-item-content>
-        <v-card
-            class="d-flex align-center justify-space-between mb-2"
-            flat
-            tile
-        >
-          <div class="text-overline ">
-            Product
-          </div>
-          <div class="text-overline ">
-            {{ $store.getters['product/getProduct'].id }}
-          </div>
-        </v-card>
-        <v-list-item-title class="text-h5 mb-1">
-          {{ $store.getters['product/getProduct'].name }}
-        </v-list-item-title>
-        <v-list-item-subtitle>{{ $store.getters['product/getProduct'].stock }}</v-list-item-subtitle>
-      </v-list-item-content>
-
-    </v-list-item>
-
     <v-card
-        class="d-flex justify-center align-center mb-2 " flat tile
+        class="d-flex align-center flex-column mx-auto pa-4 mb-1"
+        max-width="400px"
+        min-height="300px"
+        outlined
+        elevation="18"
     >
-      <v-text-field
-          v-if="title"
-          class="mb-4"
-          v-model="amount"
-          label="Refill or Decrease Stock"
-          light
-          flat
-          type="number"
-
-      ></v-text-field>
-      <v-btn
-          :disabled="!amount"
-          class="mx-2" fab dark small color="teal" @click="refill"
+      <v-list-item three-line
+                   class="mb-12"
       >
-        <v-icon dark>
-          mdi-plus
-        </v-icon>
-      </v-btn>
+        <v-list-item-content>
+          <v-card
+              class="d-flex align-center justify-space-between mb-2"
+              flat
+              tile
+          >
+            <div class="text-overline ">
+              Product
+            </div>
+            <div class="text-overline ">
+              {{ $store.getters['product/getProduct'].id }}
+            </div>
+          </v-card>
+          <v-list-item-title class="text-h5 mb-1">
+            {{ $store.getters['product/getProduct'].name }}
+          </v-list-item-title>
+          <v-list-item-subtitle>{{ $store.getters['product/getProduct'].stock }}</v-list-item-subtitle>
+        </v-list-item-content>
 
+      </v-list-item>
 
-      <v-btn
-          :disabled="!amount"
-          class="mx-2" fab dark small color="pink" @click="decreaseAmount"
+      <v-card
+          class="d-flex justify-center align-center mb-2 " flat tile
       >
-        <v-icon dark>
-          mdi-minus
-        </v-icon>
+        <v-text-field
+            v-if="title"
+            class="mb-4"
+            v-model="amount"
+            label="Refill or Decrease Stock"
+            light
+            flat
+            type="number"
 
-      </v-btn>
+        ></v-text-field>
+        <v-btn
+            :disabled="!amount"
+            class="mx-2" fab dark small color="teal" @click="refill"
+        >
+          <v-icon dark>
+            mdi-plus
+          </v-icon>
+        </v-btn>
+
+
+        <v-btn
+            :disabled="!amount"
+            class="mx-2" fab dark small color="pink" @click="decreaseAmount"
+        >
+          <v-icon dark>
+            mdi-minus
+          </v-icon>
+
+        </v-btn>
+      </v-card>
+      <div
+          v-if="!title"
+
+          class="search-something"
+      >
+        <p>Search Products</p>
+        <v-icon
+            x-large
+            color="green darken-2"
+        >
+          mdi-magnify
+        </v-icon>
+      </div>
+
     </v-card>
-    <div
-        v-if="!title"
-
-        class="search-somthing"
-    >
-    <p>Search Products</p>
-      <v-icon
-          x-large
-          color="green darken-2"
-      >
-        mdi-magnify
-      </v-icon>
-    </div>
-
-  </v-card>
 
 
     <v-card
@@ -89,17 +89,18 @@
         elevation="18"
     >
 
-        <v-text-field
-            class="mb-2"
-            v-model.number="id"
-            label="Enter Id"
-            light
-            flat
-            clearable
-            clear-icon="mdi-close-circle-outline"
-            @keyup.enter="search"
+      <v-text-field
+          autofocus
+          class="mb-2"
+          v-model.number="id"
+          label="Enter Id"
+          light
+          flat
+          clearable
+          clear-icon="mdi-close-circle-outline"
+          @keyup.enter="search"
 
-        ></v-text-field>
+      ></v-text-field>
 
       <v-btn
           :disabled="!id"
@@ -114,7 +115,6 @@
       </v-btn>
 
 
-
     </v-card>
   </div>
 
@@ -126,7 +126,7 @@ import ProductService from "@/services/ProductService";
 export default {
   name: 'Home',
   data() {
-    return {title: null, id:null, amount:null}
+    return {title: null, id: null, amount: null}
   },
   mounted() {
 
@@ -138,7 +138,7 @@ export default {
             .then(() => {
               this.title = this.$store.getters['product/getProduct']
               this.id = null
-              this.amount=null;
+              this.amount = null;
             })
 
       } catch (e) {
@@ -181,18 +181,19 @@ export default {
   },
 
 
+
 }
 </script>
 
 
 <style lang="scss">
 
-.search-somthing{
+.search-something {
   text-align: center;
   position: absolute;
-  top:70%;
-  left:50%;
-  transform: translate(-50%,-50%);
+  top: 70%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   opacity: 50%;
 }
 
